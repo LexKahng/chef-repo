5ce50889e984c169bbd2e3e5e2f9ca7302516b52
root <root@lexkahng1.mylabserver.com>
2018-06-24 03:56:06 +0000

    add database coobkoob

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -1,18 +1,17 @@
-#
 # Cookbook:: postgresql
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-package 'postgre-server' do
-	notifies :run, 'execute[postgresql-init]'
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]', :immediately
 end
 
-execute 'postresql-init' do
+execute 'postgresql-init' do
 	command 'postgresql-setup initdb'
 	action :nothing
 end
 
-service '[pstgresql' do
+service 'pstgresql' do
 	action [:enable, :start]
 end
